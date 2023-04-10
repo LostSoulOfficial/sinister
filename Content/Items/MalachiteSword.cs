a661b59ca850950d74c044144cafec496473b151
ten18a <123708109+ten18a@users.noreply.github.com>
2023-04-10 16:19:38 +0300

    Change namespace.

--- Content/Items/MalachiteSword.cs
+++ Content/Items/MalachiteSword.cs
@@ -3,7 +3,7 @@ using Terraria.ID;
 using Terraria.ModLoader;
 using Microsoft.Xna.Framework;
 
-namespace sinister.Content.Items
+namespace Sinister.Content.Items
 {
 	public class MalachiteSword : ModItem
 	{
